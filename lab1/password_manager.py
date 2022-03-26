a9b012dba295cd99c7a408c3fa9e920a22297e23
klukas17 <mihael.milicevic3@gmail.com>
2022-03-26 17:19:53 +0100

    minor improvements

--- lab1/password_manager.py
+++ lab1/password_manager.py
@@ -1,22 +1,24 @@
 #!/usr/bin/env python3
 
-# pokreni chmod +x test.py
-
-from pydoc import plain
 from sys import argv
 from Crypto.Random import get_random_bytes
-from Crypto.Protocol.KDF import PBKDF2
-from Crypto.Hash import SHA512
+from Crypto.Protocol.KDF import scrypt
 from Crypto.Cipher import AES
 
 SALT_UPPER_BOUND = 32 # len = 32
 NONCE_UPPER_BOUND = 44 # len = 12
 TAG_UPPER_BOUND = 60 # len = 16
 
+SPACES = 4
+
 def verify_args():
     if len(argv) <= 1:
         print("Missing arguments! First argument can be init, put, get.")
         exit(1)
+    elif argv[1] == "help":
+        if len(argv) != 2:
+            print("Wrong arguments! Arguments for the help command must be in 'help' format.")
+            exit(1)
     elif argv[1] == "init":
         if len(argv) != 3:
             print("Wrong arguments! Arguments for the init command must be in 'init {master_password}' format.")
@@ -29,6 +31,14 @@ def verify_args():
         if len(argv) != 4:
             print("Wrong arguments! Arguments for the put command must be in 'get {master_password} {address}' format.")
             exit(1)
+    elif argv[1] == "count":
+        if len(argv) != 3:
+            print("Wrong arguments! Arguments for the count command must be in 'count {master_password}' format.")
+            exit(1)
+    elif argv[1] == "list":
+        if len(argv) != 3:
+            print("Wrong arguments! Arguments for the list command must be in 'list {master_password}' format.")
+            exit(1)
     else:
         print("Wrong arguments! First argument can be init, put, get.")
         exit(1)
@@ -37,13 +47,32 @@ def write(master_password, data):
     f = open('vault', 'wb')
 
     salt = get_random_bytes(32)
-    key = PBKDF2(password=master_password, salt=salt, dkLen=32, count=1000000, hmac_hash_module=SHA512) 
+    key = scrypt(master_password, salt, 16, N=2**18, r=8, p=1)
     nonce = get_random_bytes(12)
     cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
     ciphertext, tag = cipher.encrypt_and_digest(data)
 
     f.write(salt + nonce + tag + ciphertext)
     f.close()
+
+def help():
+    print("Hello! Supported operations are:")
+    print()
+    print(f"{SPACES * ' '}init {{master_password}}")
+    print(f"{SPACES * 2 * ' '}initialize and set the master password")
+    print()
+    print(f"{SPACES * ' '}put {{master_password}} {{address}} {{password}}")
+    print(f"{SPACES * 2 * ' '}add a new record")
+    print()
+    print(f"{SPACES * ' '}get {{master_password}} {{address}}")
+    print(f"{SPACES * 2 * ' '}fetch password for the specified address")
+    print()
+    print(f"{SPACES * ' '}count {{master_password}}")
+    print(f"{SPACES * 2 * ' '}count the number of records")
+    print()
+    print(f"{SPACES * ' '}list {{master_password}}")
+    print(f"{SPACES * 2 * ' '}list all the records")
+    print()
 
 def init():
     write(master_password=argv[2], data=b"Password manager")
@@ -52,7 +81,11 @@ def init():
 def check():
     master_password = argv[2]
 
-    f = open('vault', 'rb')
+    try:
+        f = open('vault', 'rb')
+    except FileNotFoundError:
+        print("Vault file not found. Please use the init command to create a new vault.")
+        exit(1)
     d = f.read()
     salt = d[:SALT_UPPER_BOUND]
     nonce = d[SALT_UPPER_BOUND:NONCE_UPPER_BOUND]
@@ -60,7 +93,7 @@ def check():
     ciphertext = d[TAG_UPPER_BOUND:]
 
     try:
-        key = PBKDF2(password=master_password, salt=salt, dkLen=32, count=1000000, hmac_hash_module=SHA512) 
+        key = scrypt(master_password, salt, 16, N=2**18, r=8, p=1)
         cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
         plain_text = cipher.decrypt_and_verify(ciphertext, tag)
         records = plain_text.decode('utf-8').split('\n')[1:]
@@ -84,7 +117,7 @@ def put():
         text += f'\n{site} {password_dictionary[site]}'
     write(master_password=argv[2], data=str.encode(text))
 
-    print(f'Stored password for {address}.')
+    print(f'Stored password for {address}')
     
     
 def get():
@@ -93,15 +126,31 @@ def get():
 
     try:
         password = password_dictionary[address]
-        print(f'Password for {address} is: {password}.')
+        print(f'Password for {address} is: {password}')
     except KeyError:
         print(f'Password for {address} was never saved.')
 
+def count():
+    password_dictionary = check()
+    print(f'There are {len(password_dictionary)} passwords saved.')
+
+def list():
+    password_dictionary = check()
+    print("Saved passwords:")
+    for key in password_dictionary:
+        print(f"{SPACES * ' '}{key} - {password_dictionary[key]}")
+
 if __name__ == "__main__":
     verify_args()
-    if argv[1] == "init":
+    if argv[1] == "help":
+        help()
+    elif argv[1] == "init":
         init()
     elif argv[1] == "put":
         put()
     elif argv[1] == "get":
         get()
+    elif argv[1] == "count":
+        count()
+    elif argv[1] == "list":
+        list()
--- lab1/run.sh
+++ lab1/run.sh
@@ -1,43 +1,68 @@
 #!/bin/bash
 
 # omogući pokretanje aplikacije
-chmod +x password_manager.py
+chmod u+x password_manager.py
+
+# upute za korištenje
+./password_manager.py help
 
 # inicijalizacija
 ./password_manager.py init glavna_lozinka
 
-# dodavanje zapisa
-./password_manager.py put glavna_lozinka www.fer.hr ferlozinka
+# dodavanje nekoliko zapisa
+./password_manager.py put glavna_lozinka fer.unizg.hr ferlozinka
+./password_manager.py put glavna_lozinka moodle.fer.hr moodlelozinka
+./password_manager.py put glavna_lozinka ferko.fer.hr ferkolozinka
+./password_manager.py put glavna_lozinka sczg.unizg.hr sczglozinka
+./password_manager.py put glavna_lozinka srce.unizg.hr srcelozinka
 
-# dodavanje još jednog zapisa
-./password_manager.py put glavna_lozinka www.stranica.hr lozinka2
+# dohvaćanje broja zapisa
+./password_manager.py count glavna_lozinka
+
+# ispis svih zapisa
+./password_manager.py list glavna_lozinka
 
 # dohvaćanje zapisa koji postoji
-./password_manager.py get glavna_lozinka www.fer.hr
+./password_manager.py get glavna_lozinka fer.unizg.hr
+
+# dohvaćanje drugog zapisa koji postoji
+./password_manager.py get glavna_lozinka moodle.fer.hr 
 
 # dohvaćanje zapisa koji ne postoji
-./password_manager.py get glavna_lozinka www.ferrrr.hr
+./password_manager.py get glavna_lozinka ffzg.unizg.hr
 
-# dohvaćanje zapisa za stranicu www.stranica.hr
-./password_manager.py get glavna_lozinka www.stranica.hr
+# dohvaćanje zapisa za ferko.fer.hr
+./password_manager.py get glavna_lozinka ferko.fer.hr
 
-# promjena zapisa za www.stranica.hr
-./password_manager.py put glavna_lozinka www.stranica.hr lozinka7
+# promjena zapisa za ferko.fer.hr
+./password_manager.py put glavna_lozinka ferko.fer.hr ferkonovalozinka
 
-# ponovno dohvaćanje zapisa za www.stranica.hr
-./password_manager.py get glavna_lozinka www.stranica.hr
+# ponovno dohvaćanje zapisa za ferko.fer.hr
+./password_manager.py get glavna_lozinka ferko.fer.hr
 
 # dohvaćanje zapisa korištenjem krive glavne lozinke
-./password_manager.py get main_pasword www.fer.hr
+./password_manager.py get kriva_lozinka1 fer.unizg.hr
 
 # dodavanje zapisa korištenjem krive glavne lozinke
-./password_manager.py put main_pasword www.stranica.hr ffzglozinka
+./password_manager.py put kriva_lozinka2 ffzg.unizg.hr ffzglozinka
+
+# dohvaćanje broja zapisa korištenjem krive glavne lozinke
+./password_manager.py count kriva_lozinka3
+
+# ispis svih zapisa korištenjem krive glavne lozinke
+./password_manager.py list kriva_lozinka4
 
 # promjena datoteke, tj. narušavanje integriteta
-echo "narusi integritet" >> vault
-
-# pokušaj dodavanja zapisa nakon što je narušen integritet
-./password_manager.py put glavna_lozinka ffzg.unizg.hr tvzlozinka
+echo "narusi_integritet" >> vault
 
 # pokušaj dohvaćanja zapisa nakon što je narušen integritet
-./password_manager.py get glavna_lozinka www.fer.hr
+./password_manager.py get glavna_lozinka srce.unizg.hr
+
+# pokušaj dodavanja zapisa nakon što je narušen integritet
+./password_manager.py put glavna_lozinka ffzg.unizg.hr ffzglozinka
+
+# obriši datoteku s lozinkama
+rm vault
+
+# pokušaj dohvatiti neku lozinku nakon brisanja datoteke s lozinkama
+./password_manager.py get glavna_lozinka fer.unizg.hr
